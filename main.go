5e8a4143dcefa68c57d35b4ea73f0c9f8ab0e06d
Raghu Udiyar <raghusiddarth@gmail.com>
2020-12-28 20:15:50 +0530

    Add exclude and include to filter by regex
    Regex filtering is applied against the complete metric name and tags
    based on the Prometheus exposition format, allowing filtering both
    on metric name as well as tags, for e.g.
    
    -include-regex 'go_gc_duration_seconds{quantile="0\..?.?"}'

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -115,6 +116,47 @@ func CreateInfluxMetrics(samples model.Vector, metricPrefix string) string {
 	}
 
 	return metrics
+}
+
+func FilterSamples(samples model.Vector, includeRegex string, excludeRegex string) (model.Vector, error) {
+	var reInclude, reExclude *regexp.Regexp
+	var err error
+
+	if includeRegex != "" {
+		reInclude, err = regexp.Compile(includeRegex)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if excludeRegex != "" {
+		reExclude, err = regexp.Compile(excludeRegex)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	var filteredSamples model.Vector
+	for _, sample := range samples {
+		metricString := sample.Metric.String()
+
+		var matchInclude, matchExclude bool
+		if reInclude == nil {
+			// include all metrics
+			matchInclude = true
+		} else {
+			matchInclude = reInclude.MatchString(metricString)
+		}
+
+		if reExclude != nil {
+			matchExclude = reExclude.MatchString(metricString)
+		}
+
+		if matchInclude == true && matchExclude == false {
+			filteredSamples = append(filteredSamples, sample)
+		}
+	}
+	return filteredSamples, nil
 }
 
 func OutputMetrics(samples model.Vector, outputFormat string, metricPrefix string) error {
@@ -239,6 +281,8 @@ func main() {
 	promURL := flag.String("prom-url", "http://localhost:9090", "Prometheus API URL.")
 	queryString := flag.String("prom-query", "up", "Prometheus API query string.")
 	outputFormat := flag.String("output-format", "influx", "The check output format to use for metrics {influx|graphite|json}.")
+	includeRegex := flag.String("include-regex", "", "Regex to include metrics applied agasint the metric in Prometheus exposition format")
+	excludeRegex := flag.String("exclude-regex", "", "Regex to exclude metrics, applied after -include-regex")
 	metricPrefix := flag.String("metric-prefix", "", "Metric name prefix, only supported by line protocol output formats.")
 	insecureSkipVerify := flag.Bool("insecure-skip-verify", false, "Skip TLS peer verification.")
 	flag.Parse()
@@ -264,6 +308,14 @@ func main() {
 	} else {
 		samples, err = QueryPrometheus(*promURL, *queryString)
 
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(2)
+		}
+	}
+
+	if *includeRegex != "" || *excludeRegex != "" {
+		samples, err = FilterSamples(samples, *includeRegex, *excludeRegex)
 		if err != nil {
 			log.Fatal(err)
 			os.Exit(2)
